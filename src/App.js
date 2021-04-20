30aaa5137a81cb7eb08a63faa8e413e519793688
trezz <anahimana3@gmail.com>
2021-04-20 16:58:07 +0200

    cake reducer done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-import React ,{useState}from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
-
+import React from 'react'
+import CakeContainer from './components/CakeContainer'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 const App = () => {
-    const [showAdd, setShowAdd] = useState(false)
-    const [tasks, setTasks] = useState([
-        {
-            "id": 1,
-            "text": "Doctors Appointment",
-            "day": "Feb 5th at 2:30pm",
-            "reminder": true
-          },
-          {
-            "id": 2,
-            "text": "Meeting at School",
-            "day": "Feb 6th at 1:30pm",
-            "reminder": true
-          },
-          {
-              "id": 3,
-              "text": "Teach at dev",
-              "day": "Feb 6th at 1:30pm",
-              "reminder": true
-          }
-    ])
-
-    // on AddTask
-
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000) + 1
-        const newTask = {...task,id}
-        setTasks([...tasks,newTask])
-    }
-
-    // on delete
-
-    const onDelete = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id))
-    } 
-
-    // on toggle
-
-    const onToggle = (id) =>{
-        setTasks(tasks.map((task)=> task.id === id ?{...task, reminder :!task.reminder} : task))
-    }
+ 
     return (
-        <div className="container">
-            
-            <Header title="Tasks Tracker" onAdd={()=>setShowAdd(!showAdd)} showAdd={showAdd} />
-            { showAdd && <AddTask onAddTask={addTask}  />}
-            { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={onDelete} onToggle={onToggle} />:'No tasks to show'}
-        </div>
+        <Provider store={store}>
+            <div className="container"> 
+                <CakeContainer/>
+            </div>
+        </Provider>
     )
 }
 
---
+++ src/components/CakeContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { buyCake } from '../redux/cake/cakeActions'
+
+function CakeContainer() {
+    const numOfCakes = useSelector(state => state.numOfCakes)
+    const dispatch = useDispatch()
+    return (
+        <div>
+            <h2>number of cakes - {numOfCakes}</h2>
+            <button className="btn"
+                onClick={()=>dispatch(buyCake())}
+            >Buy cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
